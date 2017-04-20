7ff541c7fea54a57ef4e6f1bf40a39b8e7f26c2d
muri.budiman <muri.budiman@redawning.com>
2017-04-20 14:21:29 +0700

    http request example

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,34 @@
 // src/components/App/index.js
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import Request from 'react-http-request';
 
 import './style.css';
+
+class GetData extends Component {
+  render() {
+    return (
+    <Request
+        url='https://api.github.com/users/mbasso'
+        method='get'
+        accept='application/json'
+        verbose={true}
+      >
+        {
+          ({error, result, loading}) => {
+            if (loading) {
+              return <div>loading...</div>;
+            } else if(result) {
+              return <div>{JSON.stringify(result.text)}</div>;
+            } else {
+              return <div>{JSON.stringify(result)}</div>;
+            }
+          }
+        }
+      </Request>
+    );
+  }
+}
 
 class App extends Component {
   static propTypes = {}
@@ -14,7 +40,7 @@ class App extends Component {
     return (
       <div className={classnames('App', className)} {...props}>
         <div className="container-fluid">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            <GetData />
         </div>
       </div>
     );
